efb0116532e3549dbde58490ca97ab7449d88902
Matthew Cowan <mpc2145@hotmail.com>
2012-12-03 23:16:09 -0500

    more searching fake code

--- js/yelp_search.js
+++ js/yelp_search.js
@@ -1,23 +1,31 @@
 http://api.yelp.com/v2/search?term=food&location=San+Francisco
 
 
-
+var auth = {
+  //
+  // Team Rocket's auth tokens
+  //
+  consumerKey: "pcHhvaUcNovVPNbgYGpjAg",
+  consumerSecret: "GePUUz4H8LjdAi_yC1mq61nEPDo",
+  accessToken: "nB4-uHNGRrIipZCpNka6uMt5MoNFkVxl",
+  // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+  // You wouldn't actually want to expose your access token secret like this in a real application.
+  accessTokenSecret: "FvIpS9FAx_zJAru9XJ_1wU4qGT4",
+  serviceProvider: {
+    signatureMethod: "HMAC-SHA1"
+  }
+};
 
 /*
  * Construct the URL to call for the API request
  */
 function constructYelpURL() {
-    var mapBounds = map.getBounds();
     var URL = "http://api.yelp.com/" +
         "business_review_search?"+
         "callback=" + "handleResults" +
-        "&term=" + document.getElementById("term").value +
+        "&term=" + document.getElementById("search-query").value +
         "&limit=10" +
-        "&tl_lat=" + mapBounds.getSouthWest().lat() +
-        "&tl_long=" + mapBounds.getSouthWest().lng() +
-        "&br_lat=" + mapBounds.getNorthEast().lat() +
-        "&br_long=" + mapBounds.getNorthEast().lng() +
-        "&ywsid=" + YWSID;
+
     return encodeURI(URL);
 }
 
