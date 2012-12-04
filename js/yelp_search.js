6d6a4f3e5a5449cc239e4249c3e0b746cc40c3c5
Matthew Cowan <mpc2145@hotmail.com>
2012-12-03 23:37:28 -0500

    searching fucking yelp

--- js/yelp_search.js
+++ js/yelp_search.js
@@ -1,50 +1,85 @@
 http://api.yelp.com/v2/search?term=food&location=San+Francisco
 
-
+//our prelimary authentication info from yelp
 var auth = {
-  //
-  // Team Rocket's auth tokens
-  //
   consumerKey: "pcHhvaUcNovVPNbgYGpjAg",
   consumerSecret: "GePUUz4H8LjdAi_yC1mq61nEPDo",
   accessToken: "nB4-uHNGRrIipZCpNka6uMt5MoNFkVxl",
-  // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
-  // You wouldn't actually want to expose your access token secret like this in a real application.
   accessTokenSecret: "FvIpS9FAx_zJAru9XJ_1wU4qGT4",
   serviceProvider: {
     signatureMethod: "HMAC-SHA1"
   }
 };
 
+var debug_location = "New York";
+var debug_query = "pizza"
+
 /*
  * Construct the URL to call for the API request
  */
+/*
 function constructYelpURL() {
-    var URL = "http://api.yelp.com/" +
-        "business_review_search?"+
+    var URL = "http://api.yelp.com/v2/" +
+        "search?"+
         "callback=" + "handleResults" +
-        "&term=" + document.getElementById("search-query").value +
-        "&limit=10" +
-
+        "&term=" + debug_query +
+        "&location=" + debug_location +
+        "&limit=10";
     return encodeURI(URL);
 }
+*/
 
+function searchYelp() {
+    var accessor = {
+      consumerSecret: auth.consumerSecret,
+      tokenSecret: auth.accessTokenSecret
+    };
+    parameters = [];
+    parameters.push(['term', debug_query]);
+    parameters.push(['location', debug_location]);
+    parameters.push(['callback', 'handleResults']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+    var message = {
+      'action': 'http://api.yelp.com/v2/search',
+      'method': 'GET',
+      'parameters': parameters
+    };
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+    parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+    console.log(parameterMap);
+    $.ajax({
+      'url': message.action,
+      'data': parameterMap,
+      'cache': true,
+      'dataType': 'jsonp',
+      'jsonpCallback': 'handleResults',
+      'success': function(data, textStats, XMLHttpRequest) {
+        console.log(data);
+        var output = prettyPrint(data);
+        alert(output);
+      }
+    });
+}
 
 /*
- * If a sucessful API response is received, place
- * markers on the map.  If not, display an error.
+ * If a sucessful API response is received, display
+ * the result cards to the user
  */
 function handleResults(data) {
-    // turn off spinner animation
-    document.getElementById("spinner").style.visibility = 'hidden';
     if(data.message.text == "OK") {
         if (data.businesses.length == 0) {
             alert("Error: No businesses were found near that location");
             return;
         }
         for(var i=0; i<data.businesses.length; i++) {
-            biz = data.businesses[i];
-            createMarker(biz, new GLatLng(biz.latitude, biz.longitude), i);
+            alert(data.dusinesses[i]);
+            //biz = data.businesses[i];
+            //createMarker(biz, new GLatLng(biz.latitude, biz.longitude), i);
         }
     }
     else {
