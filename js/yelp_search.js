69aa5ea7e58844469abc7f620472adb886440659
Matthew Cowan <mpc2145@hotmail.com>
2012-12-04 01:09:14 -0500

    added YelpListing item to search js file

--- js/yelp_search.js
+++ js/yelp_search.js
@@ -1,4 +1,29 @@
-http://api.yelp.com/v2/search?term=food&location=San+Francisco
+function YelpListing () {
+    /*
+     * Example creation of a new listing object
+     *      var alisting = new YelpListing();
+     *      alisting.name = "Changing the name";
+    */
+    this.name           = "";   //Name of this business
+    this.id             = "";   //Yelp ID for this business
+    this.img_url        = "";   //URL of photo for this business
+    this.yelp_url       = "";   //URL for business page on Yelp
+    this.phone          = "";   //Phone number for this business formatted
+    this.review_count   = 0;    //Number of reviews for this business
+    this.rating         = 5;    //Rating for this business (0-5)
+    this.rating_img_url = "";   //url to starts rating for this business
+    this.address        = "";   //location.display_address from yelp
+    this.city           = "";   //City for this business
+    this.zip            = "";   //Postal code for this business
+    this.state_code     = "";   //State code for this business Ex: "NY"
+    this.snippet        = "";   //Snippet text associated with this business
+    //Provides a list of category name, alias pairs that this business is associated with. For example,
+    //[["Local Flavor", "localflavor"], ["Active Life", "active"], ["Mass Media", "massmedia"]]
+    this.categories     = [];
+    this.asString = function() {
+        return this.name + ' ' + this.id + ' testing';
+    };
+}
 
 //our prelimary authentication info from yelp
 var auth = {
