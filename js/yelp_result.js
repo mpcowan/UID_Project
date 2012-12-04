4956eaf5e35e80c988d1e1305b674cc30e0d23e8
Matthew Cowan <mpc2145@hotmail.com>
2012-12-04 00:47:59 -0500

    more js for listings

--- js/yelp_result.js
+++ js/yelp_result.js
@@ -1,7 +1,19 @@
-var listing = {
-    name: "Gotham Bar And Grill",
-    color: "red",
-    getInfo: function () {
-        return this.color + ' ' + this.type + ' apple';
-    }
+function YelpListing () {
+    this.name = "Gotham Bar And Grill"; //Name of this business
+    this.id = "";                       //Yelp ID for this business
+    this.img_url = "";                  //URL of photo for this business
+    this.yelp_url = "";                 //URL for business page on Yelp
+    this.phone = "";                    //Phone number for this business formatted
+    this.review_count = 0;              //Number of reviews for this business
+    this.rating = 5;                    //Rating for this business (0-5)
+    this.rating_img_url = "";           //url to starts rating for this business
+    this.asString = function() {
+        return this.name + ' ' + this.id + ' testing';
+    };
 }
+
+/*
+ * Example creation of a new listing object
+ *      var alisting = new YelpListing();
+ *      alisting.name = "Changing the name";
+*/
