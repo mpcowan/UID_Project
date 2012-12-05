a505301ad725d2b1495e5891813b09ee43c0ed07
Matthew Cowan <mpc2145@hotmail.com>
2012-12-05 05:47:55 -0500

    fml

--- js/default.js
+++ js/default.js
@@ -1,39 +1,5 @@
 $(document).ready(function () {
     var pinnedCards = [];
-
-    nokia.Settings.set("appId", "tS3F6tL4Vw-6Mz4o7F7s");
-    nokia.Settings.set("authenticationToken", "wW7onlgkAti0wUGXo8Y5Tw");
-    var map = new nokia.maps.map.Display(document.getElementById("map"),
-        {
-            'components': [
-                // ZoomBar provides a UI to zoom the map in & out
-                //new nokia.maps.map.component.ZoomBar(),
-                // We add the behavior component to allow panning / zooming of the map
-                //new nokia.maps.map.component.Behavior(),
-                // Creates UI to easily switch between street map satellite and terrain mapview modes
-                new nokia.maps.map.component.TypeSelector(),
-                // Creates a toggle button to show/hide public transport lines on the map
-                new nokia.maps.map.component.PublicTransport(),
-                /* Shows a scale bar in the bottom right corner of the map depicting
-                 * ratio of a distance on the map to the corresponding distance in the real world
-                 * in either kilometers or miles
-                 */
-                new nokia.maps.map.component.ScaleBar()
-                /* Positioning will show a set "map to my GPS position" UI button
-                 * Note: this component will only be visible if W3C geolocation API
-                 * is supported by the browser and if you agree to share your location.
-                 * If you location can not be found the positioning button will reset
-                 * itself to its initial state
-                 */
-                //new nokia.maps.positioning.component.Positioning(),
-                // Add ContextMenu component so we get context menu on right mouse click / long press tap
-                //new nokia.maps.map.component.ContextMenu()
-                ],
-                'zoomLevel': 15,
-                'center': [37.786138600000001, -122.40262130000001]
-        });
-    var marker = new nokia.maps.map.StandardMarker([37.786138600000001, -122.40262130000001]);
-    map.objects.add(marker);
 });
 
 function showModal(cardNum) {
@@ -61,18 +27,50 @@ function rePin(pinNum) {
     $('#pin' + pinNum.toString()).removeClass("pin-out");
 }
 
-function pin(pinID) {
+function pin(pinID, mapUnique, lat, lon) {
     //remove it from the results list
     $('#master' + pinID.toString()).remove();
     //add the card to the isotope class with pin in
     $(".isotope").append(toAdd_Cards[pinID]);
     //add the modal bit to the body
     $('body').append(toAdd_Modals[pinID]);
+    //init the map for the modal
+    nokia.Settings.set("appId", "tS3F6tL4Vw-6Mz4o7F7s");
+    nokia.Settings.set("authenticationToken", "wW7onlgkAti0wUGXo8Y5Tw");
+    var map = new nokia.maps.map.Display(document.getElementById(mapUnique.toString()),
+        {
+            'components': [
+                // ZoomBar provides a UI to zoom the map in & out
+                //new nokia.maps.map.component.ZoomBar(),
+                // We add the behavior component to allow panning / zooming of the map
+                //new nokia.maps.map.component.Behavior(),
+                // Creates UI to easily switch between street map satellite and terrain mapview modes
+                new nokia.maps.map.component.TypeSelector(),
+                // Creates a toggle button to show/hide public transport lines on the map
+                new nokia.maps.map.component.PublicTransport(),
+                /* Shows a scale bar in the bottom right corner of the map depicting
+                 * ratio of a distance on the map to the corresponding distance in the real world
+                 * in either kilometers or miles
+                 */
+                new nokia.maps.map.component.ScaleBar()
+                /* Positioning will show a set "map to my GPS position" UI button
+                 * Note: this component will only be visible if W3C geolocation API
+                 * is supported by the browser and if you agree to share your location.
+                 * If you location can not be found the positioning button will reset
+                 * itself to its initial state
+                 */
+                //new nokia.maps.positioning.component.Positioning(),
+                // Add ContextMenu component so we get context menu on right mouse click / long press tap
+                //new nokia.maps.map.component.ContextMenu()
+                ],
+                'zoomLevel': 15,
+                'center': [lat, lon]
+        });
+    var marker = new nokia.maps.map.StandardMarker([lat, lon]);
+    map.objects.add(marker);
 }
 
-
 function storeCards(cards_array) {
-
   if(typeof(Storage)!=="undefined")
     sessionStorage.cardsArray=cards_array;
   else
