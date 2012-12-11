c95f3e65e7b3c23598278e6300a04e4683406eb8
Matthew Cowan <mpc2145@hotmail.com>
2012-12-11 15:53:39 -0500

    pretty geolocation error message

--- js/geo.js
+++ js/geo.js
@@ -3,17 +3,17 @@
 window.google = window.google || {};
 google.maps = google.maps || {};
 (function() {
-  
+
   function getScript(src) {
     document.write('<' + 'script src="' + src + '"' +
                    ' type="text/javascript"><' + '/script>');
   }
-  
+
   var modules = google.maps.modules = {};
   google.maps.__gjsload__ = function(name, text) {
     modules[name] = text;
   };
-  
+
   google.maps.Load = function(apiLoad) {
     delete google.maps.Load;
     apiLoad([0.009999999776482582,[[["http://mt0.googleapis.com/vt?lyrs=m@199000000\u0026src=api\u0026hl=en-US\u0026","http://mt1.googleapis.com/vt?lyrs=m@199000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"m@199000000"],[["http://khm0.googleapis.com/kh?v=122\u0026hl=en-US\u0026","http://khm1.googleapis.com/kh?v=122\u0026hl=en-US\u0026"],null,null,null,1,"122"],[["http://mt0.googleapis.com/vt?lyrs=h@199000000\u0026src=api\u0026hl=en-US\u0026","http://mt1.googleapis.com/vt?lyrs=h@199000000\u0026src=api\u0026hl=en-US\u0026"],null,null,"imgtp=png32\u0026",null,"h@199000000"],[["http://mt0.googleapis.com/vt?lyrs=t@129,r@199000000\u0026src=api\u0026hl=en-US\u0026","http://mt1.googleapis.com/vt?lyrs=t@129,r@199000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"t@129,r@199000000"],null,[[null,0,7,7,[[[330000000,1246050000],[386200000,1293600000]],[[366500000,1297000000],[386200000,1314843700]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.17\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.17\u0026hl=en-US\u0026"]],[null,0,8,8,[[[330000000,1246050000],[386200000,1279600000]],[[345000000,1279600000],[386200000,1286700000]],[[354690000,1286700000],[386200000,1314843700]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.17\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.17\u0026hl=en-US\u0026"]],[null,0,9,9,[[[330000000,1246050000],[386200000,1279600000]],[[340000000,1279600000],[386200000,1286700000]],[[348900000,1286700000],[386200000,1302000000]],[[368300000,1302000000],[386200000,1314843700]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.17\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.17\u0026hl=en-US\u0026"]],[null,0,10,19,[[[329890840,1246055600],[386930130,1284960940]],[[344646740,1284960940],[386930130,1288476560]],[[350277470,1288476560],[386930130,1310531620]],[[370277730,1310531620],[386930130,1314843700]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.17\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.17\u0026hl=en-US\u0026"]],[null,3,7,7,[[[330000000,1246050000],[386200000,1293600000]],[[366500000,1297000000],[386200000,1314843700]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.17\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.17\u0026hl=en-US\u0026"]],[null,3,8,8,[[[330000000,1246050000],[386200000,1279600000]],[[345000000,1279600000],[386200000,1286700000]],[[354690000,1286700000],[386200000,1314843700]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.17\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.17\u0026hl=en-US\u0026"]],[null,3,9,9,[[[330000000,1246050000],[386200000,1279600000]],[[340000000,1279600000],[386200000,1286700000]],[[348900000,1286700000],[386200000,1302000000]],[[368300000,1302000000],[386200000,1314843700]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.17\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.17\u0026hl=en-US\u0026"]],[null,3,10,null,[[[329890840,1246055600],[386930130,1284960940]],[[344646740,1284960940],[386930130,1288476560]],[[350277470,1288476560],[386930130,1310531620]],[[370277730,1310531620],[386930130,1314843700]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.17\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.17\u0026hl=en-US\u0026"]]],[["http://cbk0.googleapis.com/cbk?","http://cbk1.googleapis.com/cbk?"]],[["http://khm0.googleapis.com/kh?v=67\u0026hl=en-US\u0026","http://khm1.googleapis.com/kh?v=67\u0026hl=en-US\u0026"],null,null,null,null,"67"],[["http://mt0.googleapis.com/mapslt?hl=en-US\u0026","http://mt1.googleapis.com/mapslt?hl=en-US\u0026"]],[["http://mt0.googleapis.com/mapslt/ft?hl=en-US\u0026","http://mt1.googleapis.com/mapslt/ft?hl=en-US\u0026"]],[["http://mt0.googleapis.com/vt?hl=en-US\u0026","http://mt1.googleapis.com/vt?hl=en-US\u0026"]]],["en-US","US",null,0,null,null,"http://maps.gstatic.com/mapfiles/","http://csi.gstatic.com","https://maps.googleapis.com","http://maps.googleapis.com"],["http://maps.gstatic.com/intl/en_us/mapfiles/api-3/10/18","3.10.18"],[1381878696],1.0,null,null,null,null,0,"",null,null,0,"http://khm.googleapis.com/mz?v=122\u0026",null,"https://earthbuilder.google.com","https://earthbuilder.googleapis.com"], loadScriptTime);
@@ -30,48 +30,60 @@ google.maps = google.maps || {};
         codeLatLng(lat, lng)
     }
 
-    function normalize_yql_response(response)  
-    {  
-        if (response.error)  
-        {  
-            var error = { code : 0 };  
-            handle_error(error);  
-            return;  
-        }  
+    function normalize_yql_response(response)
+    {
+        if (response.error)
+        {
+            var error = { code : 0 };
+            handle_error(error);
+            return;
+        }
 
-        var position = {  
-            coords :  
-            {  
-                latitude: response.place.centroid.latitude,  
-                longitude: response.place.centroid.longitude  
-            },  
-            address :  
-            {  
-                city: response.place.locality2.content,  
-                region: response.place.admin1.content,  
-                country: response.place.country.content  
-            }  
-        };  
+        var position = {
+            coords :
+            {
+                latitude: response.place.centroid.latitude,
+                longitude: response.place.centroid.longitude
+            },
+            address :
+            {
+                city: response.place.locality2.content,
+                region: response.place.admin1.content,
+                country: response.place.country.content
+            }
+        };
 
-        handle_geolocation_query(position);  
+        handle_geolocation_query(position);
     }
 
-function handle_errors(error)  
-{  
-    switch(error.code)  
-    {  
-        case error.PERMISSION_DENIED: alert("user did not share geolocation data");  
-        break;  
+function handle_errors(error)
+{
+    switch(error.code)
+    {
+        case error.PERMISSION_DENIED: bootbox.dialog("It seems you didn't share location data. Please share when prompted by your browser.", [{
+                                "label" : "OK",
+                                "class" : "btn-warning",
+                            }]);
+        break;
 
-        case error.POSITION_UNAVAILABLE: alert("could not detect current position");  
-        break;  
+        case error.POSITION_UNAVAILABLE: bootbox.dialog("Unable to determine your location. Please try again.", [{
+                                "label" : "OK",
+                                "class" : "btn-warning",
+                            }]);
+        break;
 
-        case error.TIMEOUT: alert("retrieving position timedout");  
-        break;  
+        case error.TIMEOUT: bootbox.dialog("Unable to determine your location. Please try again.", [{
+                                "label" : "OK",
+                                "class" : "btn-warning",
+                            }]);
+        break;
 
-        default: alert("unknown error");  
-        break;  
-    }  
+        default: bootbox.dialog("Unable to determine your location. Please try again.", [{
+                                "label" : "OK",
+                                "class" : "btn-warning",
+                            }]);
+        break;
+    }
 }
 
 
@@ -85,7 +97,7 @@ function handle_errors(error)
     geocoder.geocode({'latLng': latlng}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
       console.log(results)
-        if (results[1]) 
+        if (results[1])
         {
          //formatted address
          local = results[0].address_components[7].short_name;
@@ -98,7 +110,7 @@ function handle_errors(error)
                 {
                     local = results[0].address_components[i].short_name;
                 }
-              
+
             }
         }
 
@@ -109,10 +121,18 @@ function handle_errors(error)
         $("input#locale").val(local);
 
         } else {
-          alert("No results found");
+          bootbox.dialog("Unable to determine your location. Please try again.", [{
+                                "label" : "OK",
+                                "class" : "btn-warning",
+                            }]);
+          //alert("No results found");
         }
       } else {
-        alert("Geocoder failed due to: " + status);
+        bootbox.dialog("Unable to determine your location. Please try again.", [{
+                                "label" : "OK",
+                                "class" : "btn-warning",
+                            }]);
+        //alert("Geocoder failed due to: " + status);
       }
     });
   }
